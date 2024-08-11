165f3efcedab9bafa6a73b242ea521043f9237fb
Srinidhi-GR <168341432+Srinidhi-GR@users.noreply.github.com>
2024-08-11 17:55:16 +0530

    Add files via upload

---
+++ src/main/java/com/example/fragments/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn = findViewById(R.id.button);
+        Button btn2 = findViewById(R.id.button2);
+
+        btn.setOnClickListener(v->{
+            loadFragment(new FirstFragment());
+        });
+        btn2.setOnClickListener(v->{
+            loadFragment(new SecondFragment());
+        });
+    }
+    public void loadFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frameLayout, fragment);
+        ft.commit();
+    }
+}
